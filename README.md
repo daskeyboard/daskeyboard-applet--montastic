3772c86713dac1232054c89acc737d860ddc14b4
Cory Kim <github@cory.kim>
2018-10-12 17:06:17 -0500

    init(): Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Reports on monitors from Montastic
+
+This script displays CPU usage as a progress bar on 
+[`,1,2,3,4,5,6,7,8,9] keys of a Das Keyboard Q Series.
+
+![alt text](./assets/q-cpu-usage.png "Q CPU usage")
+## Requirements
+
+Install [yarn](https://yarnpkg.com/en/) and [node](https://nodejs.org/en/)
+
+[Das Keyboard Q](https://daskeyboard.io/get-started/download) running 
+
+## Installation
+
+Innstall the dependencies by running
+`yarn`
+
+## Usage 
+
+`yarn start`
---
+++ index.js
@@ -0,0 +1,72 @@
+const q = require('daskeyboard-applet');
+const request = require('request-promise');
+
+const serviceUrl = 'https://www.montastic.com/checkpoints/index';
+
+const responseColors = {
+  "0": '#FFFF00',
+  "1": '#00FF00',
+  "-1": '#FF0000'
+}
+
+const responseEffects = {
+  "0": q.Effects.BOUNCING_LIGHT,
+  "1": q.Effects.SET_COLOR,
+  "-1": q.Effects.BLINK
+}
+
+
+class QMontastic extends q.DesktopApp {
+  constructor() {
+    super();
+
+    this.serviceHeaders = {
+      "Content-Type": "application/json",
+      "X-API-KEY": this.config.authorization.apiKey,
+    }
+
+  }
+  /** ping Montastic and set the signal  */
+  async run() {
+    request.get({
+        url: serviceUrl,
+        headers: this.serviceHeaders,
+        json: true
+      }).then((body) => {
+        let points = null;
+        if (this.config.monitors) {
+          points = new Array(this.config.monitors.length);
+        } else {
+          points = [];
+        }
+
+        for (let monitor of body) {
+          // extract the important values from the response
+          let status = monitor.status;
+          let monitorId = monitor.id;
+
+          console.log(`For monitor ${monitorId}, got status: ${status}`);
+
+          let point = new q.Point(responseColors[status],
+            responseEffects[status]);
+          if (this.config.monitors) {
+            let i = this.config.monitors.indexOf(monitorId);
+            if (i >= 0) {
+              points[i] = point;
+            }
+          } else {
+            points.push(point);
+          }
+        }
+
+        return new q.Signal([points]);
+      })
+      .catch(function (error) {
+        console.error("Got error sending request to service:", error);
+      });
+  }
+}
+
+
+const montastic = new QMontastic();
+montastic.start();
