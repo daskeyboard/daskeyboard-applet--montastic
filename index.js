1d0e6679d6daa43e9e072ca1c666a591641843a2
Cory Kim <github@cory.kim>
2018-10-15 19:22:42 -0500

    feat(signal): Smarter signal processing, reduce to one point

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ class QMontastic extends q.DesktopApp {
             if (status === -1) {
               color = '#FF0000';
               alerts.push(monitor.name + " is down!");
-            } else if (this.lastMonitors[monitorId]) {
+            } else if (null !== this.lastMonitors[monitorId]) {
               alerts.push(monitor.name + " is back up.");
             }
             this.lastMonitors[monitorId] = status;
